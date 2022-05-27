893ff33c21312380c85651c724d15fa97d458e1d
daeeun1 <eodms090421@gmail.com>
2022-05-27 19:33:18 +0900

    로그인 기능 구현

--- src/com/kor/java/ssg/controller/MemberController.java
+++ src/com/kor/java/ssg/controller/MemberController.java
@@ -13,6 +13,7 @@ public class MemberController extends Controller {
 	private Scanner sc;
 	private String command;
 	private String actionMethodName;
+	private Member loginedMember; // 로그인 된 맴버
 	
 	public MemberController( Scanner sc){
 		this.sc = sc;
@@ -29,10 +30,18 @@ public class MemberController extends Controller {
 		case "join":
 			doJoin();
 			break;
+		case "login":
+			doLogin();
+			break;
+		default :
+			System.out.println("존재하지 않는 명령어 입니다.");
+			break;
 		}
 		
 	}
 	
+	
+
 	private boolean isJoinableLoginId(String loginId) {
 		int index = getMemberIndexById(loginId);
 
@@ -53,7 +62,40 @@ public class MemberController extends Controller {
 
 		return -1;
 	}
+	
+	public Member getMemberById(String loginId) {
+		int index = getMemberIndexById(loginId);
 
+		if (index == -1) {
+			return null;
+		}
+		
+		return members.get(index);
+	}
+
+	private void doLogin() {
+		System.out.printf("로그인 아이디 : ");
+		String loginId = sc.nextLine();
+		System.out.printf("로그인 비번 : ");
+		String loginIdPw = sc.nextLine();
+		
+		Member member = getMemberById(loginId);
+		
+		if (member == null) {
+			System.out.println("해당회원은 존재하지 않습니다.");
+			return;
+		}
+		
+		if (member.loginIdPw.equals(loginIdPw) == false) {
+			System.out.println("비밀번호를 확인해주세요.");
+			return;
+		}
+		
+		loginedMember = member;
+		System.out.printf("로그인 성공!, %s님 환영합니다.\n", loginedMember.name);
+		
+	}
+	
 	public void doJoin() {
 
 		int id = members.size() + 1;
